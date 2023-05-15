a80d85a206c93fada00edfa0e3a03f188dd0925f
J3sUs_78 <122793285+J3sUs78@users.noreply.github.com>
2023-05-15 19:14:02 -0400

    Day 1 - first advances

---
+++ Day1/font.cpp
@@ -0,0 +1,16 @@
+//primer programa C++
+
+#include<iostream>
+
+//
+
+
+int main()
+{
+    int numero;
+    //Array in C++
+    int numeros[5] = { 1, 2, 3, 4, 5 };
+    numero = 5 + 5;
+    return 0;
+}
+
---
+++ Day1/main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+/*
+operadores relacionales: 
+    ==(igual) !=(distinto) 
+    <(menor que) 
+    >(mayor que) 
+    <=(menor o igual que) 
+    >=(mayor o igual que)
+*/
+
+
+int main()
+{
+    int i; double d; char c;
+    cout<<"hola mundo";
+    return 0;
+
+}
